5be125758ddde8a62f4ec88db7d53323cffbf420
unknown <Flex@Flex-PC.(none)>
2012-06-20 09:07:44 +0200

    Initial Commit

---
+++ src/de/age/hellogooglemaps2/HalloGoogleMaps2Activity.java
@@ -0,0 +1,29 @@
+package de.age.hellogooglemaps2;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HalloGoogleMaps2Activity extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        
+        MapController mapController ;
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
