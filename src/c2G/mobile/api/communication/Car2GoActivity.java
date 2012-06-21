87e695ae44a649d7afa68ed9a928b7d9665686d0
ladysman237 <majestic12m@googlemail.com>
2012-06-21 11:42:43 +0200

    commit

---
+++ src/c2G/mobile/api/communication/Car2GoActivity.java
@@ -0,0 +1,20 @@
+package c2G.mobile.api.communication;
+
+import Car2Go.mobile.api.communication.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Car2GoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Endpoint ep = new Endpoint();
+//        ep.getAllFreeVehicles("ulm", "AdrianMarsch");
+//        ep.getAllParkingSpots("ulm", "AdrianMarsch");
+//        ep.getAllLocations("AdrianMarsch");
+        ep.getAllPGasStations("ulm", "AdrianMarsch");
+        
+    }
+}
